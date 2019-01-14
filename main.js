efcb7d98f3a4f47760e123a90c8e90a4e5bfaf29
Maiwand <mhussain2@spartaglobal.com>
2019-01-14 23:48:11 +0000

    added second version of calculator

--- main.js
+++ main.js
@@ -1,16 +1,80 @@
 console.log('working');
 
-var name = prompt("What is your name?");
 
-function outerFunction(goblin) {
-  
-   function innerFunction(troll) {
-     console.log(troll);
+// var start = "yes";
+// while (start === "yes") {
+//   var name = prompt("What is your name?");
+//   var num1 = prompt('First number');
+//   var num2 = prompt('Second number');
+//   var choice = prompt('Would you like to add, subtract, times or divide?');
+// function calculate(arg1, arg2, operator) {
+//   if (operator === "add")
+//   return parseInt(arg1) + parseInt(arg2)
+//   if (operator === "subtract")
+//   return parseInt(arg1) - parseInt(arg2)
+//   if (operator === "times")
+//   return parseInt(arg1) * parseInt(arg2)
+//   if (operator === "divide")
+//   return parseInt(arg1) / parseInt(arg2) 
+// } 
 
-         }
 
-   innerFunction(goblin)            
+// var sum = calculate(num1, num2, choice);
+// alert(`${sum}`); 
+// var end = prompt("Would you like to continue?");
+// if (end === "no") {
+//   break;
+// } 
 
-}
+// }
 
-outerFunction(name);
+
+
+
+
+function calc() {
+  var num1 = prompt(`First Number`);
+  var num2 = prompt(`Second Number`);
+  var choice = prompt(`+, -, *, /`);
+} if (choice === '+') {
+  return Number(num1) + Number(num2);
+} if (choice === '-') {
+  return Number(num1) - Number(num2);
+} if (choice === '*') {
+  return Number(num1) + Number(num2);
+} if (choice === '/') {
+  return Number(num1) / Number(num2);
+} 
+console.log( calc());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
